62cf6f4cb542b0f0e93064cd71bb084f3e6f3cb9
sunny <torbjrno@gmail.com>
2019-08-22 18:56:57 +0200

    Made a check for comic #404 so the app wont crash.

--- app/src/main/java/com/example/sunny/xkcd_app/MainActivity.kt
+++ app/src/main/java/com/example/sunny/xkcd_app/MainActivity.kt
@@ -18,11 +18,10 @@ import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    var index = 1 // Index decides what comic should be displayed
-    var lastComicNr = 0 // Helpfull for stopping index becoming higher then the max nr(stopping 404 errors)
+    private var index = 1 // Index decides what comic should be displayed
+    private var lastComicNr = 0 // Helpfull for stopping index becoming higher then the max nr(stopping 404 errors)
     // Also helpful when checks for new comics gets implemented.
-
-    var editText: EditText? = null; // Used for searching by comicNr and displaying current comicNr
+    private var editText: EditText? = null; // Used for searching by comicNr and displaying current comicNr
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         Glide.with(this)
             .load(url)
+            .override(500,500)
             .into(imageView)
     }
 
@@ -59,10 +59,13 @@ class MainActivity : AppCompatActivity() {
         //Gets an URL based on current index.
         //Displays the URL as an image
 
+        if (index == lastComicNr)return; // Makes sure nothing happens when pressing the button when on the last comic
+
         var url: String = "";
 
         val getURL = GetWebAPI()
         url = getURL.execute(index).get()
+        if(index == 403)index++; // comicnr 404 returns a 404 error this check will send index from 403->405
         index++
         editText?.setText(Integer.toString(index))
 
@@ -78,9 +81,12 @@ class MainActivity : AppCompatActivity() {
         //Gets an URL based on current index.
         //Displays the URL as an image
 
+        if (index <= 1) return; // Makes sure you cant press the button when it is at the first comic or for some reason(This should not be possilbe) lower
+
         var url: String = "";
         val getURL = GetWebAPI()
         url = getURL.execute(index).get()
+        if(index==405)index--; // comicnr 404 returns a 404 error this check will send index from 405->403
         index--
         editText?.setText(Integer.toString(index))
 
@@ -98,7 +104,7 @@ class MainActivity : AppCompatActivity() {
 
         index = Integer.parseInt(editText?.text.toString())
 
-        if (index > lastComicNr || index == 0) return
+        if (index > lastComicNr || index == 0 || index == 404) return
 
         var url: String = "";
         val getURL = GetWebAPI()
